63eeb47bad46bdbeecbc8ea28799317f8fc9c349
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2025-02-14 18:38:23 +0500

    fix: test gh-page deployment

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,4 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      fallback: "404.html",
-    }),
-  },
 };
