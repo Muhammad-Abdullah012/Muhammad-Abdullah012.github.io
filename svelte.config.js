a8153332d7a1b17c6f24c355db9b9bd8a91afff1
Muhammad Abdullah <not.muhammad.abdullah68@gmail.com>
2025-02-16 20:24:25 +0500

    testing github pages

--- svelte.config.js
+++ svelte.config.js
@@ -5,4 +5,11 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base:
+        process.env.NODE_ENV === "production" ? "/Muhammad-Abdullah012.github.io" : "",
+    },
+  },
 };
